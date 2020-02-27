fc51149dce68d5147c2b7741799858c560eb423a
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-02-27 13:45:31 +0900

    第二章(パスはまだ通っていない)

---
+++ Report2/equalizeCV.cc
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import cv2
+from grayscale import readImage
+from histogram import histImage
+from histogramCV import getHistogram
+
+
+# In[5]:
+
+def main():
+    img = readImage()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Gray', gray)
+    histSize = 256
+    hist = histImage(getHistogram(gray, histSize))
+    cv2.imshow('Gray Histogram', hist)
+    equal = cv2.equalizeHist(gray)
+    cv2.imshow('Result', equal)
+    hist = histImage(getHistogram(equal, histSize))
+    cv2.imshow('Result Histogram', hist)
+    cv2.waitKey(0)
+    cv2.imwrite('equalized.jpg', equal)
+    cv2.destroyAllWindows()
+
+# In[8]:
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
